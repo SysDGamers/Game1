c0142df4af4c8f76b40b573d5b6af7dd28a1858b
yuik <yuik@yuik>
2014-10-12 20:00:01 +0900

    Updated to mariolike

--- SysD_Project/src/sysD_game/FrameInfo.java
+++
@@ -1,6 +0,0 @@
-package sysD_game;
-
-public class FrameInfo {
-	public int MainFrameWidth = 500;
-	public int MainFrameHeight = 500;
-}
--- SysD_Project/src/sysD_game/Main.java
+++ SysD_Project/src/sysD_game/Main.java
@@ -1,15 +1,37 @@
 package sysD_game;
 
+import java.awt.Container;
 
+import javax.swing.JFrame;
 
-public class Main {
+/*
+ * Created on 2005/06/06
+ *
+ */
 
-	public static void main(String[] args) {
-		//test commit (ttb)
-		
-		
-		new MainFrame();
-	}
-	
+/**
+ * @author mori
+ *
+ */
+public class Main extends JFrame {
+    public Main() {
+        // タイトルを設定
+        setTitle("マップの読み込み");
+        // サイズ変更不可
+        setResizable(false);
 
+        // メインパネルを作成してフレームに追加
+        MainPanel panel = new MainPanel();
+        Container contentPane = getContentPane();
+        contentPane.add(panel);
+
+        // パネルサイズに合わせてフレームサイズを自動設定
+        pack();
+    }
+
+    public static void main(String[] args) {
+        Main frame = new Main();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
 }
