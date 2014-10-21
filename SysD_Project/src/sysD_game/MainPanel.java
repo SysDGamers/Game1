4262bcd024ddc0996b656a650ca851e60018f025
tatebe <tatebe@192.168.100.102>
2014-10-21 20:54:41 +0900

    iでウインドウ表示 escapeで解除

--- SysD_Project/src/sysD_game/MainPanel.java
+++ SysD_Project/src/sysD_game/MainPanel.java
@@ -3,6 +3,7 @@ package sysD_game;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
@@ -15,152 +16,173 @@ import javax.swing.JPanel;
 
 /**
  * @author mori
- *  
+ *
  */
 public class MainPanel extends JPanel implements Runnable, KeyListener {
-    // パネルサイズ
-    public static final int WIDTH = 640;
-    public static final int HEIGHT = 480;
+	// パネルサイズ
+	public static final int WIDTH = 800;
+	public static final int HEIGHT = 600;
 
-    // マップ
-    private Map map;
+	// マップ
+	private Map map;
 
-    // プレイヤー
-    private Player player;
+	//ウインドウ
+	private Inventory inventory;
+	private static Rectangle WIND_RECT = new Rectangle(100, 450, 600,100);
+	// プレイヤー
+	private Player player;
 
-    // キーの状態（押されているか、押されてないか）
-    private boolean leftPressed;
-    private boolean rightPressed;
-    private boolean upPressed;
+	// キーの状態（押されているか、押されてないか）
+	private boolean leftPressed;
+	private boolean rightPressed;
+	private boolean upPressed;
+	private boolean escape;
+	// ゲームループ用スレッド
+	private Thread gameLoop;
 
-    // ゲームループ用スレッド
-    private Thread gameLoop;
+	public MainPanel() {
+		// パネルの推奨サイズを設定、pack()するときに必要
+		setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		// パネルがキー入力を受け付けるようにする
+		setFocusable(true);
 
-    public MainPanel() {
-        // パネルの推奨サイズを設定、pack()するときに必要
-        setPreferredSize(new Dimension(WIDTH, HEIGHT));
-        // パネルがキー入力を受け付けるようにする
-        setFocusable(true);
+		// マップを作成
+		map = new Map("map01.dat");
 
-        // マップを作成
-        map = new Map("map01.dat");
+		// プレイヤーを作成
+		player = new Player(192, 32, map);
 
-        // プレイヤーを作成
-        player = new Player(192, 32, map);
+		// キーイベントリスナーを登録
+		addKeyListener(this);
+		inventory = new Inventory(WIND_RECT);
 
-        // キーイベントリスナーを登録
-        addKeyListener(this);
+		// ゲームループ開始
+		gameLoop = new Thread(this);
+		gameLoop.start();
+	}
 
-        // ゲームループ開始
-        gameLoop = new Thread(this);
-        gameLoop.start();
-    }
+	/**
+	 * ゲームループ
+	 */
+	public void run() {
+		while (true) {
+			if (leftPressed) {
+				// 左キーが押されていれば左向きに加速
+				player.accelerateLeft();
+			} else if (rightPressed) {
+				// 右キーが押されていれば右向きに加速
+				player.accelerateRight();
+			} else {
+				// 何も押されてないときは停止
+				player.stop();
+			}
 
-    /**
-     * ゲームループ
-     */
-    public void run() {
-        while (true) {
-            if (leftPressed) {
-                // 左キーが押されていれば左向きに加速
-                player.accelerateLeft();
-            } else if (rightPressed) {
-                // 右キーが押されていれば右向きに加速
-                player.accelerateRight();
-            } else {
-                // 何も押されてないときは停止
-                player.stop();
-            }
+			if (upPressed) {
+				// ジャンプする
+				player.jump();
+			}
+			//
+			if(escape){
+				inventory.show();
+			}else if(!escape){
+				inventory.hide();
+			}
 
-            if (upPressed) {
-                // ジャンプする
-                player.jump();
-            }
+			// プレイヤーの状態を更新
+			player.update();
 
-            // プレイヤーの状態を更新
-            player.update();
+			// 再描画
+			repaint();
 
-            // 再描画
-            repaint();
+			// 休止
+			try {
+				Thread.sleep(20);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
-            // 休止
-            try {
-                Thread.sleep(20);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
 
-    /**
-     * 描画処理
-     * 
-     * @param 描画オブジェクト
-     */
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
+	/**
+	 * 描画処理
+	 *
+	 * @param 描画オブジェクト
+	 */
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
 
-        // 背景を黒で塗りつぶす
-        g.setColor(Color.BLACK);
-        g.fillRect(0, 0, getWidth(), getHeight());
+		// 背景を黒で塗りつぶす
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, getWidth(), getHeight());
 
-        // X方向のオフセットを計算
-        int offsetX = MainPanel.WIDTH / 2 - (int)player.getX();
-        // マップの端ではスクロールしないようにする
-        offsetX = Math.min(offsetX, 0);
-        offsetX = Math.max(offsetX, MainPanel.WIDTH - map.getWidth());
+		// X方向のオフセットを計算
+		int offsetX = MainPanel.WIDTH / 2 - (int)player.getX();
+		// マップの端ではスクロールしないようにする
+		offsetX = Math.min(offsetX, 0);
+		offsetX = Math.max(offsetX, MainPanel.WIDTH - map.getWidth());
 
-        // Y方向のオフセットを計算
-        int offsetY = MainPanel.HEIGHT / 2 - (int)player.getY();
-        // マップの端ではスクロールしないようにする
-        offsetY = Math.min(offsetY, 0);
-        offsetY = Math.max(offsetY, MainPanel.HEIGHT - map.getHeight());
+		// Y方向のオフセットを計算
+		int offsetY = MainPanel.HEIGHT / 2 - (int)player.getY();
+		// マップの端ではスクロールしないようにする
+		offsetY = Math.min(offsetY, 0);
+		offsetY = Math.max(offsetY, MainPanel.HEIGHT - map.getHeight());
 
-        // マップを描画
-        map.draw(g, offsetX, offsetY);
+		// マップを描画
+		map.draw(g, offsetX, offsetY);
 
-        // プレイヤーを描画
-        player.draw(g, offsetX, offsetY);
-    }
+		// プレイヤーを描画
+		player.draw(g, offsetX, offsetY);
 
-    /**
-     * キーが押されたらキーの状態を「押された」に変える
-     * 
-     * @param e キーイベント
-     */
-    public void keyPressed(KeyEvent e) {
-        int key = e.getKeyCode();
+		inventory.draw(g);
+	}
 
-        if (key == KeyEvent.VK_LEFT) {
-            leftPressed = true;
-        }
-        if (key == KeyEvent.VK_RIGHT) {
-            rightPressed = true;
-        }
-        if (key == KeyEvent.VK_UP) {
-            upPressed = true;
-        }
-    }
+	/**
+	 * キーが押されたらキーの状態を「押された」に変える
+	 *
+	 * @param e キーイベント
+	 */
+	public void keyPressed(KeyEvent e) {
+		int key = e.getKeyCode();
 
-    /**
-     * キーが離されたらキーの状態を「離された」に変える
-     * 
-     * @param e キーイベント
-     */
-    public void keyReleased(KeyEvent e) {
-        int key = e.getKeyCode();
+		if (key == KeyEvent.VK_LEFT) {
+			leftPressed = true;
+		}
+		if (key == KeyEvent.VK_RIGHT) {
+			rightPressed = true;
+		}
+		if (key == KeyEvent.VK_UP) {
+			upPressed = true;
+		}
+		if (key == KeyEvent.VK_I){
+			escape = true;
+		}
+		if (key == KeyEvent.VK_ESCAPE){
+			escape = false;
+		}
 
-        if (key == KeyEvent.VK_LEFT) {
-            leftPressed = false;
-        }
-        if (key == KeyEvent.VK_RIGHT) {
-            rightPressed = false;
-        }
-        if (key == KeyEvent.VK_UP) {
-            upPressed = false;
-        }
-    }
+	}
 
-    public void keyTyped(KeyEvent e) {
-    }
+	/**
+	 * キーが離されたらキーの状態を「離された」に変える
+	 *
+	 * @param e キーイベント
+	 */
+	public void keyReleased(KeyEvent e) {
+		int key = e.getKeyCode();
+
+		if (key == KeyEvent.VK_LEFT) {
+			leftPressed = false;
+		}
+		if (key == KeyEvent.VK_RIGHT) {
+			rightPressed = false;
+		}
+		if (key == KeyEvent.VK_UP) {
+			upPressed = false;
+		}
+
+	}
+
+	public void keyTyped(KeyEvent e) {
+	}
 }
